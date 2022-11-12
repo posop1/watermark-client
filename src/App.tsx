aef8f245a9f05df512fea9dc6d728f8714c0dc27
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-12 18:40:27 +0500

    add  app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
-function App() {
-  return <div className="App"></div>;
+import axios from 'axios'
+import React from 'react'
+import s from './styles/app.module.scss'
+
+const App = () => {
+  const OnSumbit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const formData = {
+      image: new FormData(event.target.form)
+    }
+    console.log(formData)
+
+    axios
+      .post('http://127.0.0.1:8000/fill/', formData)
+      .then((res) => {
+        console.log(`Success` + res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  return (
+    <div className={s.app}>
+      <h1>TEST</h1>
+      <form
+        onSubmit={OnSumbit}
+        className={s.form}
+      >
+        <input
+          accept="image/jpeg"
+          id="contained-button-content"
+          name="customFile"
+          type="file"
+          className={s.input}
+        />
+        <button className={s.btn}>Sub</button>
+      </form>
+    </div>
+  )
 }
 
-export default App;
+export default App
